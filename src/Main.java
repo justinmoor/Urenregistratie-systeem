c949d433ca847406e35578c491ce9c3b8805c939
StalenStan <stanhoenson@icloud.com>
2017-09-11 13:39:14 +0200

    123test

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("Jo");
         System.out.println("Gittest");
         System.out.println("Yoyo");
+        System.out.println("Klinklariekoek");
     }
 
 }
