c5e7a967891a565ccefebc78d7ecbd59281e8a64
StalenStan <stanhoenson@icloud.com>
2017-09-18 13:57:53 +0200

    yolo
    1 2 3 test

---
+++ src/Yolo.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Stan on 18/11/2017.
+ */
+public class Yolo {
+
+    public static void main (String[] args){
+        System.out.println("Yolo");
+        System.out.println("Yolo");
+        System.out.println("Yolo");
+        System.out.println("Yolo");
+        System.out.println("Yolo");
+        System.out.println("Yolo");
+    }
+
+}
