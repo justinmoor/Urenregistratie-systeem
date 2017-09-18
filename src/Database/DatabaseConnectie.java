ded4b9d86b9fa2b764a03e016228f382d0b85f93
Justin <justin-804@hotmail.com>
2017-09-18 11:41:31 +0200

    datasbase

--- src/Database/DatabaseConnectie.java
+++ src/Database/DatabaseConnectie.java
@@ -1,11 +1,15 @@
 package Database;
+import java.sql.*;
 
 public class DatabaseConnectie {
 
-    public DatabaseConnectie(){
-        System.out.println("Test");
+    public DatabaseConnectie() {
+
+        try{
+            Connection  connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/DB?user=root&password=ipsen123");
+        } catch (Exception ex){
+            System.out.println("Niet gevonden");
+        }
+
     }
-
-
-
 }
