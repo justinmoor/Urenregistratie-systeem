ec4cc6f53d878c657d63abe066d1837c10d6484e
Alex <s1103096@student.hsleiden.nl>
2017-10-05 10:09:55 +0200

    pullen

---
+++ src/Models/IngevuldeTijdModel.java
@@ -0,0 +1,20 @@
+package Models;
+
+public class IngevuldeTijdModel {
+
+private int uurId;
+private int personeelID;
+private String beginDatum;
+private String beginTijd;
+private String eindDatum;
+private String eindTijd;
+private String klant;
+private String project;
+private String onderwerp;
+private String commentaar;
+private boolean goedGekeurd;
+}
+
+//met commentaar
+public IngevuldeTijdModel(int uurId, int personeelId, String beginDatum, String beginTijd, String eindDatum, String eindTijd, String klant, String project, String onderwerp, String commentaar)
+    this.
