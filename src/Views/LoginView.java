aa57c8895285d2c4aa86cbe4d554449ec3623dc9
StalenStan <stanhoenson@icloud.com>
2017-10-09 09:08:39 +0200

    css login

--- src/Views/LoginView.java
+++ src/Views/LoginView.java
@@ -8,7 +8,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -18,6 +21,11 @@ public class LoginView extends Scene {
     private GridPane gridpane;
 
     private Label title;
+    
+//    private ImageView grijs;
+//    private Image lijntje;
+    
+//    private HBox hbox;
 
     private Label email;
     private Label password;
@@ -41,39 +49,57 @@ public class LoginView extends Scene {
 
     //Grafische elementen initialiseren.
     public void initGui() {
+    	   
         gridpane.setAlignment(Pos.CENTER);
-        gridpane.setHgap(20);
+        gridpane.setHgap(15);
         gridpane.setVgap(15);
-        gridpane.setPadding(new Insets(25, 25, 25, 25));
         gridpane.setStyle("-fx-background-color: #f9f9f7");
+        
+        getStylesheets().add("Views/styles.css");
 
+//        hbox.setPrefHeight(27);
+//        hbox.setPrefWidth(144);
+        
         title = new Label("Urenregistratie");
         title.setId("title");
-        title.setFont(Font.font("SansSerif", FontWeight.BOLD, 30));
-        title.setTextFill(Color.GREY);
         gridpane.add(title, 0, 0, 2, 1);
+        
+//        lijntje = new Image("lijntje.png");
+//        grijs = new ImageView();
+//        grijs.setImage(lijntje);
+//        gridpane.add(grijs, 0, 1);
 
         email = new Label("E-Mail:");
+        email.setId("emailLabel");
         email.setTextFill(Color.GREY);
         gridpane.add(email, 0, 1);
 
         email_input = new TextField();
+        email_input.setId("emailTextfield");
+//        email_input.setMinHeight(hbox.getPrefHeight());
+//        email_input.setMinWidth(hbox.getPrefWidth());
         gridpane.add(email_input, 1, 1);
+        
+//        hbox.getChildren().addAll(email, email_input);
 
         password = new Label("Wachtwoord:");
+        password.setId("passwordLabel");
         password.setTextFill(Color.GREY);
         gridpane.add(password, 0, 2);
 
         password_input = new PasswordField();
+        password_input.setId("passwordTextfield");
         gridpane.add(password_input, 1, 2);
 
-        login = new Button("Log in");
-        gridpane.add(login, 0, 3);
+        login = new Button("LOG IN");
+        login.setId("loginButton");
+        login.setAlignment(Pos.CENTER);
+        gridpane.add(login, 1, 3);
 
         feedback = new Label("");
         gridpane.add(feedback, 0, 4, 4, 1);
 
-        this.getStylesheets().add("Views/styles.css");
+        
     }
 
     //Functionele zaken initialiseren.
