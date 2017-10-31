76f42237cd599bb42ed35d9a009d0d891408e23d
Alex <s1103096@student.hsleiden.nl>
2017-10-31 13:18:04 +0100

    timePicker mooi maken merged

--- src/Views/TimePicker.java
+++ src/Views/TimePicker.java
@@ -3,7 +3,6 @@ package Views;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
